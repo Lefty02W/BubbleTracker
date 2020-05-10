10ec2580cb00f5811d997f43884e25868cdfd8bf
Luke Walsh <lwa383@uclive.ac.nz>
2020-05-10 23:22:30 +1200

    Linked main buttons
    - personal card button linked to view
    - business card btn linked to view
    - scanner btn linked to view

--- app/src/main/java/com/example/bubbletracker/MainActivity.kt
+++ app/src/main/java/com/example/bubbletracker/MainActivity.kt
@@ -1,12 +1,33 @@
 package com.example.bubbletracker
 
 import android.app.Activity
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
 
 class MainActivity : Activity(){
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val btnOpenPersonalCardActivity: Button = findViewById(R.id.personalCardBtn)
+        val btnOpenBusinessCardActivity: Button = findViewById(R.id.businessCardBtn)
+        val btnOpenScannerActivity: Button = findViewById(R.id.scannerBtn)
+
+        btnOpenPersonalCardActivity.setOnClickListener {
+            val openPersonalCard = Intent(this, PersonalCardActivity::class.java)
+            startActivity(openPersonalCard)
+        }
+
+        btnOpenBusinessCardActivity.setOnClickListener {
+            val openBusinessCardActivity = Intent(this, BusinessCardActivity::class.java)
+            startActivity(openBusinessCardActivity)
+        }
+
+        btnOpenScannerActivity.setOnClickListener {
+            val openScannerActivity = Intent(this, ScannerActivity::class.java)
+            startActivity(openScannerActivity)
+        }
     }
 }
