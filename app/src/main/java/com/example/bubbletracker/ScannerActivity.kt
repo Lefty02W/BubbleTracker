47d3df83202f2c572273f8adcaeb479e30f2db54
Luke Walsh <lwa383@uclive.ac.nz>
2020-05-22 13:31:40 +1200

    Added scanner

--- app/src/main/java/com/example/bubbletracker/ScannerActivity.kt
+++ app/src/main/java/com/example/bubbletracker/ScannerActivity.kt
@@ -1,35 +1,56 @@
 package com.example.bubbletracker
 
 import android.app.Activity
+import android.content.Intent
 import android.os.Bundle
 import android.widget.Toast
-import me.dm7.barcodescanner.zbar.Result
-import me.dm7.barcodescanner.zbar.ZBarScannerView
+import com.google.zxing.integration.android.IntentIntegrator
 
-class ScannerActivity: Activity(), ZBarScannerView.ResultHandler {
+//import me.dm7.barcodescanner.zbar.Result
+//import me.dm7.barcodescanner.zbar.ZBarScannerView
 
-    private lateinit var mScannerView: ZBarScannerView
+class ScannerActivity: Activity(){
+
+//    private lateinit var mScannerView: ZBarScannerView
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        mScannerView = ZBarScannerView(this)
-//        setContentView(R.layout.activity_scanner)
-        setContentView(mScannerView)
+//        mScannerView = ZBarScannerView(this)
+        setContentView(R.layout.activity_scanner)
+        val scanner = IntentIntegrator(this)
+        scanner.setDesiredBarcodeFormats(IntentIntegrator.QR_CODE)
+        scanner.setBeepEnabled(false)
+        scanner.initiateScan()
+//        setContentView(mScannerView)
     }
 
-    override fun onResume() {
-        super.onResume()
-        mScannerView.startCamera()
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        if (resultCode == Activity.RESULT_OK) {
+            val result = IntentIntegrator.parseActivityResult(requestCode, resultCode, data)
+            if (result != null) {
+                if (result.contents == null) {
+                    Toast.makeText(this, "Cancelled", Toast.LENGTH_LONG).show()
+                } else {
+                    Toast.makeText(this, "Scanned: " + result.contents, Toast.LENGTH_LONG).show()
+                }
+            } else {
+                super.onActivityResult(requestCode, resultCode, data)
+            }
+        }
     }
+//    override fun onResume() {
+//        super.onResume()
+//       // mScannerView.startCamera()
+//    }
 
-    override fun onPause() {
-        super.onPause()
-        mScannerView.stopCamera()
-    }
+//    override fun onPause() {
+//        super.onPause()
+//       // mScannerView.stopCamera()
+//    }
 
-    override fun handleResult(rawResult: Result?) {
-        Toast.makeText(this, rawResult?.contents, Toast.LENGTH_SHORT).show()
-        mScannerView.resumeCameraPreview(this)
-    }
+//    override fun handleResult(rawResult: Result?) {
+//        Toast.makeText(this, rawResult?.contents, Toast.LENGTH_SHORT).show()
+//        mScannerView.resumeCameraPreview(this)
+//    }
 }
