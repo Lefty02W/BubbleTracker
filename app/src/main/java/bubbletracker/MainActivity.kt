dbd3eff76b2be0faa19fcea7d33bcb79d4c91792
Luke Walsh <lwa383@uclive.ac.nz>
2020-05-25 21:57:02 +1200

    Added animation

--- app/src/main/java/bubbletracker/MainActivity.kt
+++ app/src/main/java/bubbletracker/MainActivity.kt
@@ -3,7 +3,10 @@ package bubbletracker
 import android.app.Activity
 import android.content.Intent
 import android.os.Bundle
+import android.view.animation.Animation
+import android.view.animation.AnimationUtils
 import android.widget.Button
+import android.widget.TextView
 import android.widget.Toast
 import com.example.bubbletracker.R
 import com.google.zxing.integration.android.IntentIntegrator
@@ -13,6 +16,8 @@ class MainActivity : Activity(){
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        runAnimation()
 
         val btnOpenPersonalCardActivity: Button = findViewById(R.id.personalCardBtn)
         val btnOpenBusinessCardActivity: Button = findViewById(R.id.businessCardBtn)
@@ -50,4 +55,38 @@ class MainActivity : Activity(){
             }
         }
     }
+
+    override fun onResume() {
+        runAnimation()
+        super.onResume()
+    }
+
+    override fun onStart() {
+        runAnimation()
+        super.onStart()
+    }
+
+    private fun runAnimation() {
+        val btt: Animation = AnimationUtils.loadAnimation(this, R.anim.btt)
+        val btt2: Animation = AnimationUtils.loadAnimation(this, R.anim.btt2)
+        val btt3: Animation = AnimationUtils.loadAnimation(this, R.anim.btt3)
+
+        val personalCardBtn: TextView = findViewById(R.id.personalCardBtn)
+        val businessCardBtn: TextView = findViewById(R.id.businessCardBtn)
+        val scannerBtn: TextView = findViewById(R.id.scannerBtn)
+        personalCardBtn.startAnimation(btt3)
+        businessCardBtn.startAnimation(btt2)
+        scannerBtn.startAnimation(btt)
+//        val ttb2: Animation = AnimationUtils.loadAnimation(this, R.anim.ttb2)
+//        val stb: Animation = AnimationUtils.loadAnimation(this, R.anim.stb)
+
+//        val logo: ImageView = findViewById(R.id.logoImage)
+//        val rudimentBtn: TextView = findViewById(R.id.rudiment_btn)
+//        val metronomeBtn: TextView = findViewById(R.id.metronome_btn)
+//
+//        logo.startAnimation(stb)
+//        rudimentBtn.startAnimation(ttb)
+//        metronomeBtn.startAnimation(ttb2)
+    }
+
 }
